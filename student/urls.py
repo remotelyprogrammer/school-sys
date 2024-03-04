eb08511c63f8e8f1d00bde026ea635f7c1bd4c6b
remotelyprogrammer <remotelyprogrammer@gmail.com>
2024-03-04 17:12:18 +0800

    new feature

--- student/urls.py
+++ student/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('create/', views.CreateStudentView.as_view(), name='create-student'),
     path('', home, name='student-home'),
     path('<int:pk>/add-contacts/', add_student_contacts, name='add-contacts'),
+    path('<int:pk>/', views.StudentDetailView.as_view(), name='student-detail'),
 
 ]
