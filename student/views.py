73a97b7d24b0809769981ffb4bbf3134bb0b7911
remotelyprogrammer <remotelyprogrammer>
2024-03-06 00:53:53 +0800

    sending to another pc

--- student/views.py
+++ student/views.py
@@ -6,7 +6,7 @@ from django.urls import reverse_lazy
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 
-#yes
+#test
 class StudentDeleteView(DeleteView):
     model = Student
     template_name = 'student/student-confirm-delete.html'
